e7c54bf1525898cc59f1506b8fab6ab5ad617e06
ruobinhu18 <56205329+ruobinhu18@users.noreply.github.com>
2021-10-29 04:41:14 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+/*
+import {View} from 'react-native';
+import NavDemo1 from './components/NavDemo1'
+import Quiz1 from './components/Quiz1.js'
+import Quiz1a from './components/Quiz1a.js'
+import Grading from './components/Grading.js'
+import PropDemo from './components/PropDemo.js'
+import TipCalculator from './components/TipCalculator.js'
+import Counter from './components/Counter.js'
+import ListDemoScreen from './components/ListDemoScreen'
+//import CovidDemo from './components/CovidDemo'
+import PhotoId from './components/PhotoId'
+import ShowPage from './components/ShowPage'
+
+import ShowPage from './components/ShowPage'
+import Counter from './components/Counter.js'
+import WebViewer from './components/WebViewer'
+*/
+import Profile from './components/Profile'
+import Pomodoros from './components/Pomodoros'
+import WebViewer from './components/WebViewer'
+import NavDemo1 from './components/NavDemo1'
+import TipCalculator from "./components/TipCalculator";
+
+export default function App() {
+  return (
+    //<Pomodoros />
+    <NavDemo1 taxRate={0.2}/>
+    //<TipCalculator tipRate={0.2}/>
+    //<WebViewer/>
+    //<Profile />
+  );
+}
